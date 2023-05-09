4c1a2ee41a674d84020415f31dc2238b60c72e48
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2023-05-09 12:07:39 +0200

    added headers for enum - UI

---
+++ headers/enumUiAction.hpp
@@ -0,0 +1,14 @@
+#ifndef UI_ACTION_HPP
+#define UI_ACTION_HPP
+
+enum class UiAction {
+    addNewStudent = 0
+    ,showDataBase
+    ,searchBySurname
+    ,searchByPesel
+    ,sortByPesel
+    ,sortBySurname
+    ,deleteByIndexNo
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include "headers/dataBase.hpp"
 #include "headers/person.hpp"
 #include "headers/student.hpp"
-#include "headers/dataBase.hpp"
+#include "headers/userInterface.hpp"
 #include <algorithm>
 #include <cmath>
 #include <ios>
@@ -13,45 +14,45 @@
 
 
 int main() {
-    Student student1 {"Łukasz", "Meisnerowski", "male", "11111111111", "Obornicka 6/15", "64-500", "Szamotuły", "123455"};
-    Student student2 {"Katarzyna", "Januszko", "female", "22222222222", "Januszkowa 4/23", "88-666", "Wrocław", "778899"};
-    Student student3 {"Bartek", "Marciniak", "male", "33333333333", "Marciniakowa 4", "55-111", "Poznań", "556688"};
-    Student student4 {"Gustaw", "Gustawski", "male", "44444444444", "Gustawska 3", "44-331", "PomyloneGary", "112234"};
-    Student student5 {"Klementyna", "Klementyńska", "female", "55555555555", "Klementyńska 18", "88-666", "Wrocław", "156680"};
-    Student student6 {"Konrad", "Konradzki", "male", "66666666666", "Konradzka 12", "33-222", "Białystok", "459981"};
-    Student student7 {"Zuzanna", "Zuzanowska", "female", "77777777777", "Zuzannowa 66", "22-888", "Gostyń", "001287"};
+    // Student student1 {"Łukasz", "Meisnerowski", "male", "11111111111", "Obornicka 6/15", "64-500", "Szamotuły", "123455"};
+    // Student student2 {"Katarzyna", "Januszko", "female", "22222222222", "Januszkowa 4/23", "88-666", "Wrocław", "778899"};
+    // Student student3 {"Bartek", "Marciniak", "male", "33333333333", "Marciniakowa 4", "55-111", "Poznań", "556688"};
+    // Student student4 {"Gustaw", "Gustawski", "male", "44444444444", "Gustawska 3", "44-331", "PomyloneGary", "112234"};
+    // Student student5 {"Klementyna", "Klementyńska", "female", "55555555555", "Klementyńska 18", "88-666", "Wrocław", "156680"};
+    // Student student6 {"Konrad", "Konradzki", "male", "66666666666", "Konradzka 12", "33-222", "Białystok", "459981"};
+    // Student student7 {"Zuzanna", "Zuzanowska", "female", "77777777777", "Zuzannowa 66", "22-888", "Gostyń", "001287"};
 
-    std::vector<Student*> vec;
+    // std::vector<Student*> vec;
 
-    vec.push_back(&student1);
-    vec.push_back(&student2);
-    vec.push_back(&student3);
-    vec.push_back(&student4);
-    vec.push_back(&student5);
-    vec.push_back(&student6);
-    vec.push_back(&student7);
-    //sort(begin(vec), end(vec), [](Student* student1, Student* student2){return student1->getSurname() < student2->getSurname();});
-    dataBase db(vec);
-    //db.CreateStudent(vec);
-    //db.CreateStudent(vec);
-    //db.CreateStudent(vec);
-    std::cout << "Sorting by surname!\n";
-    db.sortBySurname(vec);
-    std::cout << "Print: \n";
-    db.printStudentData(vec);
-    //db.Print(vec);
-    std::cout << "Finding by surname\n";
-    db.findBySurname(vec);
-    std::cout << "Sorting by PESEL! \n";
-    db.sortByPESEL(vec);
-    std::cout << "Print: \n";
-    db.printStudentData(vec);
-    std::cout << "Finding by PESEL!\n";
-    db.findByPESEL(vec);
-    std::cout << "Delete by index no!\n";
-    db.deleteByIndexNumber(vec);
-    std::cout << "Print: \n";
-    db.printStudentData(vec);
+    // vec.push_back(&student1);
+    // vec.push_back(&student2);
+    // vec.push_back(&student3);
+    // vec.push_back(&student4);
+    // vec.push_back(&student5);
+    // vec.push_back(&student6);
+    // vec.push_back(&student7);
+    // //sort(begin(vec), end(vec), [](Student* student1, Student* student2){return student1->getSurname() < student2->getSurname();});
+    // dataBase db(vec);
+    // //db.CreateStudent(vec);
+    // //db.CreateStudent(vec);
+    // //db.CreateStudent(vec);
+    // std::cout << "Sorting by surname!\n";
+    // db.sortBySurname(vec);
+    // std::cout << "Print: \n";
+    // db.printStudentData(vec);
+    // //db.Print(vec);
+    // std::cout << "Finding by surname\n";
+    // db.findBySurname(vec);
+    // std::cout << "Sorting by PESEL! \n";
+    // db.sortByPESEL(vec);
+    // std::cout << "Print: \n";
+    // db.printStudentData(vec);
+    // std::cout << "Finding by PESEL!\n";
+    // db.findByPESEL(vec);
+    // std::cout << "Delete by index no!\n";
+    // db.deleteByIndexNumber(vec);
+    // std::cout << "Print: \n";
+    // db.printStudentData(vec);
 
 
 
